5acdae0d89f8cfdca37134751e4cadfb222beb6c
wsq003 <wsq003@gmail.com>
2015-03-14 22:05:10 +0800

    Create itoa.c

---
+++ itoa.c
@@ -0,0 +1,49 @@
+//return the length of result string. support only 10 radix for easy use and better performance
+int itoa(int val, char* buf)
+{
+    const unsigned int radix = 10;
+
+    char* p;
+    unsigned int a;        //every digit
+    int len;
+    char* b;            //start of the digit char
+    char temp;
+    unsigned int u;
+
+    p = buf;
+
+    if (val < 0)
+    {
+        *p++ = '-';
+        val = 0 - val;
+    }
+    u = (unsigned int)val;
+
+    b = p;
+
+    do
+    {
+        a = u % radix;
+        u /= radix;
+
+        *p++ = a + '0';
+
+    } while (u > 0);
+
+    len = (int)(p - buf);
+
+    *p-- = 0;
+
+    //swap
+    do
+    {
+        temp = *p;
+        *p = *b;
+        *b = temp;
+        --p;
+        ++b;
+
+    } while (b < p);
+
+    return len;
+}
